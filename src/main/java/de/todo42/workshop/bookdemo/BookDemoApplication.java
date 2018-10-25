c37bd61df535fc98be742dfc92badcce6734435f
Dominik Hirt <hirt@todo42.de>
2018-10-25 12:55:08 +0200

    first commit

---
+++ src/main/java/de/todo42/workshop/bookdemo/BookDemoApplication.java
@@ -0,0 +1,12 @@
+package de.todo42.workshop.bookdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDemoApplication.class, args);
+	}
+}
